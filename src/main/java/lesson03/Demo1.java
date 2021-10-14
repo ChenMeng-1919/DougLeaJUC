3619bc804e4f9b30ab57d5b121739a4c7c376e11
cm <chenmeng_1919@163.com>
2021-10-14 11:37:55 +0800

    lesson03

---
+++ src/main/java/lesson03/Demo1.java
@@ -0,0 +1,14 @@
+package lesson03;
+
+/*
+ * @author: cm
+ * @date: Created in 2021/10/14 11:22
+ * @description:
+ * Amdahl强调：当串行换比例一定时，加速比是有上限的，不管你堆叠多少个CPU参与计算，都不能突破这个上限。
+
+    Gustafson定律关系的是：如果可被并行化的代码所占比例足够大，那么加速比就能随着CPU的数量线性增长。
+
+    总的来说，提升性能的方法：想办法提升系统并行的比例，同时增加CPU数量。
+ */
+public class Demo1 {
+}
